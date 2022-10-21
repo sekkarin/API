6cddc68f19e43562b4aafa01bdacc11cffa15c92
sekkarin <sekkri1234@gmail.com>
2022-10-21 16:23:37 +0700

    first commit

---
+++ database.js
@@ -0,0 +1,74 @@
+const data = [
+    {
+        id: 1,
+        name:"Shinkanzen Sushi (ชินคันเซ็น ซูชิ) - GrabKitchen พาต้า",
+        type:"ซูชิ",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/3-C2EBGBLYVEJKCE/hero/9fb9822d12e04dd8ba0ccd6c08a1ddc9_1662003301308751803.jpeg"
+
+    },
+    {
+        id: 2,
+        name:"McDonald's (แมคโดนัลด์) - ราชดำเนิน",
+        type:"แฮมเบอร์เกอร์, ฟาสต์ฟู้ด, ไก่ทอด",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/AWh64KYIZXYdMpch2Zem/hero/dcf0e5d0dbee4da0b93462539366e5f5_1663573204173776022.png"
+
+    },
+    {
+        id: 3,
+        name:"Oppa Daek ไก่ทอดเกาหลี - Grabkitchen พาต้า",
+        type:"ไก่ทอด",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/3-C2EVG2JHAKBCFE/hero/7086e4bfa8df46cd8ed166381d02e6b7_1654438010984239278.jpeg"
+
+    },
+    {
+        id: 4,
+        name:"Dim sum zai ติ่มซำไจ๋ 點心仔 - แขวงคลองมหานาค",
+        type:"Coupon, ติ่มซำ",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/3-C2UTPBKJN6NHC2/hero/3af8960daa824dd4a1c068b57fbe50a7_1624524351103482801.jpeg"
+
+    },
+    {
+        id: 5,
+        name:"โชคดีติ่มซำ - ถนนบรรทัดทอง",
+        type:"Coupon, ติ่มซำ",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/THGFIST000000b7/hero/db8ec25eac2440e4a3c4f794b098358c_1604844651444314620.jpg"
+
+    },
+    {
+        id: 6,
+        name:"WokStar Express (Western Chinese Eats) - สุขุมวิทซอย6",
+        type:"Coupon, ติ่มซำ",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/3-CZMAV7VFMEKACE/hero/ef1bda2062e943cda37517abd0cb717b_1600318885967939575.jpeg"
+
+    },
+    {
+        id: 7,
+        name:"ตี๋เย็นตาโฟรสเด็ด - สำราญราษฎร์",
+        type:"Coupon, อาหารเส้น",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/3-CZMTKBDTUFNCG2/hero/ae7af5115bca4144b9e5ff89c21809f9_1614767032838057861.jpeg"
+
+    },
+    {
+        id: 8,
+        name:"ขนมจีนเจ๊เฉย - เพชรบุรี",
+        type:"อาหารเส้น",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/THGFIST000000j5/hero/8f06163ea4374a41909f8f9fbaa425fa_1601820574713078548.jpeg"
+
+    },
+    {
+        id: 9,
+        name:"ยำแรด - ถนนศรียุธยา",
+        type:"Coupon, อาหารทะเล",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/3-C2JZGKNVRA6TNE/hero/0f703de80323496aa5d1f31ebd7e29a4_1636557279264314555.jpeg"
+
+    },
+    {
+        id: 10,
+        name:"คุณนายทะเลดอง -วังใหม่",
+        type:"Coupon, อาหารทะเล",
+        ImageURL: "https://d1sag4ddilekf6.azureedge.net/compressed/merchants/3-C2LZNYXWSCLDTX/hero/d2d2086eb2194848b54b97c9626cdbd7_1615791593057999731.jpg"
+
+    },
+
+]
+module.exports = data
